e8708b71891eb61e819849d464829b410583c09f
Amit Verma <amitfromus@gmail.com>
2025-05-06 22:20:03 -0700

    added seo content

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
-export const APP_BASE_URL = process.env.NODE_ENV == 'development' ? "http://localhost:3000" : "https://inbits--streettadka.us-central1.hosted.app"
+export const APP_BASE_URL = process.env.NODE_ENV == 'development' ? "http://localhost:3000" : "https://www.inbits.co"
 
 
 export const NEWSAPI_END_POINT = "https://newsapi.org";
